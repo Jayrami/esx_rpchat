07321d0383f6163e650520f39041e825ae78c713
Jay <72338591+Jayrami@users.noreply.github.com>
2021-07-05 08:32:46 +0300

    Update en.lua

--- esx_rpchat/locales/en.lua
+++ esx_rpchat/locales/en.lua
@@ -1,11 +1,13 @@
 Locales['en'] = {
   ['ooc_prefix'] = 'OOC | %s',
   ['twt_help'] = 'send a tweet',
-  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['twt_prefix'] = 'Twitter (@%s)',
   ['me_help'] = 'personal action',
   ['me_prefix'] = 'me | %s',
   ['do_help'] = 'RP information',
   ['do_prefix'] = 'do | %s',
   ['generic_argument_name'] = 'message',
   ['generic_argument_help'] = 'the message',
+  ['ad_prefix'] = '^1Advertisement ^0| %s',
+  ['system_prefix'] = '^1SYSTEM:',
 }
